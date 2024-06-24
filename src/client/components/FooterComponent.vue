0310bf4a6e6ebe41e6f9455ed20041864eb84952
Jasmin Bajric <jasmin.bajric@stud.fh-campuswien.ac.at>
2024-06-24 21:03:00 +0200

    Add Points to FooterComponent

--- src/client/components/FooterComponent.vue
+++ src/client/components/FooterComponent.vue
@@ -2,14 +2,14 @@
 </script>
 
 <template>
-  <footer class="fixed bottom-0 left-0 w-full text-center flex flex-col items-center space-y-0.5">
+  <footer class="fixed bottom-0 left-0 w-full text-center flex flex-col items-center space-y-0.5 mt-10">
     <p>&copy; 2024 Cooking Club. Alle Rechte vorbehalten.</p>
     <div class="flex justify-between space-x-2">
-      <p><a href="#">Home</a></p>
-      <p><a href="#">Impressum</a></p>
-      <p><a href="#">Datenschutz</a></p>
-      <p><a href="#">Über uns</a></p>
-      <p><a href="#">Kontakt</a></p>
+      <p><a href="/dashboard">Home</a></p>
+      <p><a href="/imprint">Impressum</a></p>
+      <p><a href="/legal">Datenschutz</a></p>
+      <p><a href="/about-us">Über uns</a></p>
+      <p><a href="/contact">Kontakt</a></p>
     </div>
   </footer>
 </template>
